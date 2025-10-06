b82c44adab50eece8878874b62594772eb91a848
MartinCorsalini <martincorsalini@outlook.com>
2025-10-06 20:24:45 -0300

    estructuracion de carpetas e imagenes

---
+++ src/app/components/shared/client-logo/client-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-logo.component.html',
+  styleUrl: './client-logo.component.css'
+})
+export class ClientLogoComponent {
+
+}
