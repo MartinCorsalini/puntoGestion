87665ec8f2cbe2d21418c67682e1b43f734086e3
MartinCorsalini <martincorsalini@outlook.com>
2025-10-08 20:58:47 -0300

    empezando con el footer

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  products = [
+    'Tango Gestión',
+    'Tango Punto de Venta',
+    'Tango Estudios Contables',
+    'Tango Resto',
+    'Tango Factura'
+  ];
 
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
